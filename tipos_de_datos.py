535720e105a159558723bdefd34210e90cc463ee
fabians48 <fabians48@gmail.com>
2021-02-17 19:23:20 +0000

    .

--- tipos_de_datos.py
+++ tipos_de_datos.py
@@ -90,9 +90,9 @@ apellidos_tuple = tuple(apellidos_list[49:22:-2])
 (con las conversiones que sean necesarias) y agregarle el elemento de la 
 posición 78 de la lista `apellidos_list`.
 """
-apellidos_list2 = list(apellidos_tuple) #+ apellidos_list[78]
-#apellidos_list2 = apellidos_list[78]
-#print(apellidos_list2)
+apellidos_list2 = list(apellidos_tuple)
+apellidos_list2.append(apellidos_list[78])
+print(apellidos_list2)
 
 """Eliminar el 4 elemento de la variable `apellidos_list2`.
 """
